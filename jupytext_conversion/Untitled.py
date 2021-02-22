bc403e946548faac602fadbf0381996389643666
Daniel Deutsch <deudan1010@gmail.com>
2021-02-22 18:44:24 +0100

    update

---
+++ jupytext_conversion/Untitled.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.10.2
+#   kernelspec:
+#     display_name: crypto_art_data_conda
+#     language: python
+#     name: crypto_art_data_conda
+# ---
+
+print('example')
+
+test
+
+
+
--- readme.md
+++ readme.md
@@ -1,4 +1,3 @@
 # Data science boilerplate example
 
-This is a boilerplate example referenced in my article https://github.com/Createdd/Writing/blob/master/2021/articles/dataBoilerplate.md
-
+This is a boilerplate example referenced in my article https://towardsdatascience.com/how-to-start-a-data-science-project-boilerplate-in-2021-33d81393e50
