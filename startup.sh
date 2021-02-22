205e3cabb5749478bb64e4953fbef4dc2b2fac80
Daniel Deutsch <deudan1010@gmail.com>
2021-02-22 19:44:51 +0100

    Fix bundle script

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 jupyter notebook &
-when-changed requirements_dev.in pip-compile requirements_dev.in;pip install -r requirements_dev.txt
+when-changed requirements_dev.in pip-compile requirements_dev.in &
+when-changed requirements_dev.in pip install -r requirements_dev.txt
